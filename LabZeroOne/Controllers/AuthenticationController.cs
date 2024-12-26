ac321706eea68c3c9ac88a01393c83bed42f0dde
tampv <phamtampt2k1@gmail.com>
2024-12-26 22:48:29 +0700

    login form

---
+++ LabZeroOne/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabZeroOne.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabZeroOne/Controllers/NotionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabZeroOne.Controllers
+{
+    public class NotionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
